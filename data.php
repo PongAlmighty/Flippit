6074dbef285d93ed470ef969b8d169a80420f217
TheMightyPong <6806519+PongAlmighty@users.noreply.github.com>
2022-08-07 19:44:33 +0000

    close enough for now...

--- data.php
+++ data.php
@@ -1,8 +1,18 @@
 <?php
 $strVar = $_GET['strVar'];
-$foo = "hello";
-  if ($strVar=="2"){
-          $foo = "world";
+$foo = "No Var Rcved";
+$str1 = "123456789012345";
+$str2 = "Howdy";
+$str3 = "NEW SUB";
+  if ($strVar=="1"){
+          $foo = $str1;
+  } elseif 
+    ($strVar=="2"){
+          $foo = $str2;
+  } elseif 
+    ($strVar=="3"){
+          $foo = $str3;
   }
+
 print $foo
 ?>
--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    
+    <meta http-equiv="refresh" content="6">
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width">
@@ -16,77 +16,79 @@
 
 <style>
 .tick {
-  font-size:1rem; white-space:nowrap; font-family:arial,sans-serif;
-}
-
-.tick-flip,.tick-text-inline {
-  font-size:2.5em;
+  font-size:2.0rem; 
+  white-space:nowrap; 
+  font-family:arial,sans-serif;
+  text-align:horizontal,vertical-text;
+  
 }
 
 .tick-label {
-  margin-top:1em;font-size:1em;
+  margin-top:.01em;
+  font-size:3em;
+  color: #595d63 !important; 
+  
 }
 
 .tick-char {
-  width:1.5em;
+  width:.5em;
+  font-size:2em; 
+  text-align:center;
 }
 
 .tick-text-inline {
   display:inline-block;text-align:center;min-width:1em;
-}
-
-.tick-text-inline+.tick-text-inline {
-  margin-left:-.325em;
+  color: #595d63 !important; 
+  
+  font-size:1em;
 }
 
 .tick-group {
-  margin:0 .5em;text-align:center;
+  margin:0 5.5em;text-align:center;
 }
 
 body {
    background-color: #fff !important; 
 }
 
-.tick-text-inline {
-   color: #595d63 !important; 
-}
-
-.tick-label {
-   color: #595d63 !important; 
-}
-
 .tick-flip-panel {
    color: #fff !important; 
+   background-color: #3c3e3c !important; 
+  font-size:1.5em;
+    margin:0 0;
 }
 
+  
 .tick-flip {
-   font-family: !important; 
+  font-family: !important; 
+  margin-left: .0625em;
+  margin-right: .0625em;
+  border-radius:0.12em !important;  
+  font-size:3em;
 }
 
 .tick-flip-panel-text-wrapper {
-   line-height: 1.45 !important; 
+   line-height: 1em !important; 
+  text-align:center;
+   
 }
 
-.tick-flip-panel {
-   background-color: #3c3e3c !important; 
-}
 
-.tick-flip {
-   border-radius:0.12em !important;  
+
 }
 </style>
 
-  <div class="tick" data-did-init="handleTickInit">
+  <div class="tick" data-did-init="handleTickInit" data-layout="horizontal fit">
 
-    <div data-repeat="true" data-layout="horizontal fit" data-transform="upper -> split -> delay(random, 100, 150)">
-        <span data-view="flip" data-transform="ascii -> arrive -> round -> char(a-zA-Z)" class="tick-char"></span>
+    <div data-repeat="true" data-layout="horizontal" class="tick-flip-panel" data-transform="upper -> split -> delay(random, 100, 150)" >
+        <span data-overlay="center" data-view="flip" data-transform="ascii -> arrive -> char(a-zA-Z0-9)"></span>
     </div>
   </div>
 
-    <div class="tick2" data-did-init="handleTickInit2">
+    <div class="tick" data-did-init="handleTickInit2">
 
     <div data-repeat="true" data-layout="horizontal fit" data-transform="upper -> split -> delay(random, 100, 150)">
-        <span data-view="flip" data-transform="ascii -> arrive -> round -> char(a-zA-Z)" class="tick-char"></span>
+        <span data-view="flip" data-style="flip-easing: ease-in-circ" data-transform="ascii -> arrive -> round -> char(a-zA-Z0-9)" class="tick-char"></span>
     </div>
   </div>
 
@@ -95,32 +97,25 @@ body {
 <script>
     function handleTickInit(tick) {
         // request data from a php file every five seconds
-        Tick.data.poll(
+        Tick.data.request(
             'data.php?strVar=1',
             function(response) {
               tick.value = response;
               console.log(response);
-            },
-            5000
+            }
         );
     }
 
-    function handleTickInit2(tick2) {
+    function handleTickInit2(tick) {
         // request data from a php file every five seconds
-        Tick.data.poll(
-            'data.php?strVar=2',
+        Tick.data.request(
+            'data.php?strVar=3',
             function(response) {
               tick.value = response;
               console.log(response);
-            },
-            0
+            }
         );
     }
-
-
-
-
-
 
 </script>
 
